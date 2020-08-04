3c1d23c8245aee702c3eb1d1f2c941d22d45443a
grrazzlle <cel47@miami.edu>
2020-08-04 19:23:19 -0400

    fixing some html

---
+++ script.js
@@ -0,0 +1,19 @@
+//api key = b77386ac30431512966d88af2b144f30
+////api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q={city name}
+////api.openweathermap.org/data/2.5/uvi/forecast?appid=b77386ac30431512966d88af2b144f30&lat={lat}&lon={lon}
+////api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat={lat}&lon={lon}
+
+//on first loadup set current city to miami
+//get history from local storage
+//on search button click take input value and prepend to list of history and add to local storage
+//take most recent history item and add to current weather api search
+//await response and take lat and lon and add to uv and forcast api searches
+//await all responses and display data
+//display current city and date as a header in information card
+//display temperature in fahrenheit from kalvin
+//display humididy in percent
+//display wind speed in mph
+//display uv index with color based on number
+//display 5 days of forecast in forcast card using more cards
+//display date, weather img, temp and humidity
+
