8192e2cfc2c3f3d336f2e82fe6b7e8ca0e0aedb7
grrazzlle <cel47@miami.edu>
2020-08-04 21:09:02 -0400

    added api calls

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //api key = b77386ac30431512966d88af2b144f30
-////api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q={city name}
-////api.openweathermap.org/data/2.5/uvi/forecast?appid=b77386ac30431512966d88af2b144f30&lat={lat}&lon={lon}
-////api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat={lat}&lon={lon}
+//https://api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q={city name}
+//https://api.openweathermap.org/data/2.5/uvi/forecast?appid=b77386ac30431512966d88af2b144f30&lat={lat}&lon={lon}
+//https://api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat={lat}&lon={lon}
 
 //on first loadup set current city to miami
 //get history from local storage
@@ -17,3 +17,38 @@
 //display 5 days of forecast in forcast card using more cards
 //display date, weather img, temp and humidity
 
+listGroup = document.querySelector('.list-group');
+
+document.querySelector('#searchBtn').addEventListener('click', addCityName);
+function addCityName(event) {
+  let searchCityName = document.querySelector('#searchCityName').value;
+  let li = document.createElement('li');
+  li.setAttribute('class', 'list-group-item');
+  li.setAttribute('id', searchCityName);
+  li.innerText = searchCityName;
+  listGroup.prepend(li);
+  li.addEventListener('click', cityNameSearch);
+  document.querySelector('#searchCityName').value = '';
+  
+}
+
+async function cityNameSearch(event) {
+  let cityName = this.innerText;
+  let response1 = await fetch('https://api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q=' + cityName);
+  let data1 = await response1.json();
+  console.log(data1)
+  let cityLat = data1.coord.lat;
+  let cityLon = data1.coord.lon;
+  let response2 = await fetch('https://api.openweathermap.org/data/2.5/uvi/forecast?appid=b77386ac30431512966d88af2b144f30&lat=' + cityLat +'&lon=' + cityLon);
+  let data2 = await response2.json();
+  console.log(data2)
+  let response3 = await fetch('https://api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat=' + cityLat +'&lon=' + cityLon);
+  let data3 = await response3.json();
+  console.log(data3)
+  let dataCityName = data1.name;
+  let currentCityName = document.createElement('div');
+  let currentDate = moment().format('M/D/YYYY');
+  currentCityName.innerText = dataCityName + ' ' + currentDate
+  document.querySelector('#currentWeather').append(currentCityName)
+
+};
