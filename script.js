b514fbab7423a9743b9c2cc65cff2a68b6eaaf79
grrazzlle <cel47@miami.edu>
2020-08-05 16:56:24 -0400

    mostly done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //api key = b77386ac30431512966d88af2b144f30
 //https://api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q={city name}
-//https://api.openweathermap.org/data/2.5/uvi/forecast?appid=b77386ac30431512966d88af2b144f30&lat={lat}&lon={lon}
+//https://api.openweathermap.org/data/2.5/uvi?appid=b77386ac30431512966d88af2b144f30&lat={lat}&lon={lon}
 //https://api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat={lat}&lon={lon}
 
 //on first loadup set current city to miami
@@ -18,53 +18,92 @@
 //display date, weather img, temp and humidity
 
 listGroup = document.querySelector('.list-group');
-let cityName = ''
+let history = [];
+let currentCityName = '';
+function checkLocalStorage() {
+  if (localStorage.getItem('historyKey')) {
+    history = JSON.parse(localStorage.getItem('historyKey'));
+    currentCityName = history[0];
+  } else {
+    history = ['Miami'];
+    currentCityName = history[0];
+  }
+}
+
+function renderHistory() {
+  document.querySelector('.list-group').innerHTML = '';
+  for (var i = 0; i < history.length; i++) {
+    let li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item');
+    li.setAttribute('id', history[i]);
+    li.innerText = history[i];
+    li.addEventListener('click', historyCityNameChange);
+    listGroup.append(li);
+  }
+}
+let searchCityName = '';
+
+function titleCase(searchCityName) {
+  searchCityName = searchCityName.toLowerCase().split(' ');
+  for (var i = 0; i < searchCityName.length; i++) {
+    searchCityName[i] = searchCityName[i].charAt(0).toUpperCase() + searchCityName[i].slice(1);
+  }
+  return searchCityName.join(' ');
+}
+
+function cityNameCheck(event) {
+  for (var i = 0; i < history.length; i++) {
+    if (searchCityName == history[i]) {
+      history.splice(i, 1);
+    }
+  }
+  history.unshift(searchCityName);
+}
 
 document.querySelector('#searchBtn').addEventListener('click', addCityName);
 function addCityName(event) {
-  let searchCityName = document.querySelector('#searchCityName').value;
-  let li = document.createElement('li');
-  li.setAttribute('class', 'list-group-item');
-  li.setAttribute('id', searchCityName);
-  cityName = searchCityName;
-  li.innerText = searchCityName;
-  listGroup.prepend(li);
-  li.addEventListener('click', historyCityNameChange);
+  searchCityName = document.querySelector('#searchCityName').value;
+  searchCityName = titleCase(searchCityName);
+  cityNameCheck();
   document.querySelector('#searchCityName').value = '';
+  currentCityName = history[0];
+  renderHistory();
   apiCall();
 }
 
 function historyCityNameChange() {
-  cityName = this.innerText;
+  searchCityName = this.innerText;
+  cityNameCheck();
+  currentCityName = history[0];
+  renderHistory();
   apiCall();
 };
 
 
+
+
 async function apiCall(event) {
-  let response1 = await fetch('https://api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q=' + cityName);
+  let response1 = await fetch('https://api.openweathermap.org/data/2.5/weather?appid=b77386ac30431512966d88af2b144f30&q=' + currentCityName);
   let data1 = await response1.json();
-  console.log(data1)
   let cityLat = data1.coord.lat;
   let cityLon = data1.coord.lon;
-  let response2 = await fetch('https://api.openweathermap.org/data/2.5/uvi/forecast?appid=b77386ac30431512966d88af2b144f30&lat=' + cityLat +'&lon=' + cityLon);
+  let response2 = await fetch('https://api.openweathermap.org/data/2.5/uvi?appid=b77386ac30431512966d88af2b144f30&lat=' + cityLat + '&lon=' + cityLon);
   let data2 = await response2.json();
-  console.log(data2)
-  let response3 = await fetch('https://api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat=' + cityLat +'&lon=' + cityLon);
+  let response3 = await fetch('https://api.openweathermap.org/data/2.5/onecall?appid=b77386ac30431512966d88af2b144f30&exclude=current,minutely,hourly&lat=' + cityLat + '&lon=' + cityLon);
   let data3 = await response3.json();
-  console.log(data3)
 
   document.querySelector('#currentWeather').innerHTML = '';
 
   let dataCityName = data1.name;
-  let currentCity = document.createElement('div');
+  let currentCity = document.createElement('h2');
   let currentDate = moment().format('M/D/YYYY');
   currentCity.innerText = dataCityName + ' ' + currentDate;
   document.querySelector('#currentWeather').append(currentCity);
 
   let kelvin = data1.main.temp;
-  let temperature = (kelvin - 273.15) * 9/5 + 32;
+  let temperature = (kelvin - 273.15) * 9 / 5 + 32;
   let currentTemperature = document.createElement('div');
-  currentTemperature.innerText = 'Temperature: ' + temperature + ' F';
+  currentTemperature.innerText = 'Temperature: ' + temperature.toFixed(2); + ' F';
   document.querySelector('#currentWeather').append(currentTemperature);
 
   let humidity = data1.main.humidity;
@@ -77,8 +116,55 @@ async function apiCall(event) {
   currentWind.innerText = 'Wind Speed: ' + wind + ' MPH';
   document.querySelector('#currentWeather').append(currentWind);
 
-  let UV = data2[0].value;
+  let UV = data2.value;
   let currentUV = document.createElement('div');
   currentUV.innerText = 'UV Index: ' + UV;
   document.querySelector('#currentWeather').append(currentUV);
+
+  document.querySelector('#forcast').innerHTML = ''
+
+  for (var i = 0; i < 5; i++) {
+    let date = moment().add((i + 1), 'days').format('M/D/YYYY');
+
+    let col = document.createElement('div');
+    col.setAttribute('class', 'col');
+    document.querySelector('#forcast').append(col);
+
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card text-white bg-primary');
+    col.append(card);
+
+    let cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body text-center');
+    card.append(cardBody);
+
+    let cardHead = document.createElement('h5');
+    cardHead.setAttribute('class', 'card-title');
+    cardHead.innerText = date;
+    cardBody.append(cardHead);
+
+
+    let weatherImg = document.createElement('img');
+    let weatherId = data3.daily[i].weather[0].icon
+    weatherImg.setAttribute('src', 'http://openweathermap.org/img/wn/' + weatherId + '@2x.png');
+    cardBody.append(weatherImg);
+
+    let dailyKelvin = data3.daily[i].temp.day;
+    let dailyTemperature = (dailyKelvin - 273.15) * 9 / 5 + 32;
+    let tempDisplay = document.createElement('div');
+    tempDisplay.innerText = 'Temp: ' + dailyTemperature.toFixed(2); + ' F';
+    cardBody.append(tempDisplay);
+
+    let dailyHumidity = data3.daily[i].humidity;
+    let humidityDisplay = document.createElement('div');
+    humidityDisplay.innerText = 'Humidity: ' + dailyHumidity + ' %';
+    cardBody.append(humidityDisplay);
+
+
+    localStorage.setItem('historyKey', JSON.stringify(history));
+  }
 };
+
+checkLocalStorage();
+renderHistory();
+apiCall();
